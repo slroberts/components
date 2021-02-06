c1f5cb42445bb67200249c84b674dfc99646247a
Shomari Roberts <shomariroberts@gmail.com>
2021-02-05 23:35:51 -0500

    made CommentDetail a resuable component with props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail
+        avatar={faker.image.people()}
+        author="Sam"
+        timeAgo="Today at 4:55PM"
+        comment="Awesome. Nice blog post!"
+      />
+      <CommentDetail
+        avatar={faker.image.nightlife()}
+        author="Alex"
+        timeAgo="Today at 2:00PM"
+        comment="I like the subject."
+      />
+      <CommentDetail
+        avatar={faker.image.abstract()}
+        author="Jane"
+        timeAgo="Yesterday at 6:30PM"
+        comment="Cool!"
+      />
     </div>
   );
 };
